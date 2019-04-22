8aa8815ba65eb587b067b30b251f826fb45e09e5
Gregor Tätzner <mail@gtaetzner.de>
2019-04-22 19:35:42 +0200

    add dokku config

--- apps/mep_memory_web/config/prod.exs
+++ apps/mep_memory_web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # before starting your production server.
 config :mep_memory_web, MepMemoryWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("WEB_HOST"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
